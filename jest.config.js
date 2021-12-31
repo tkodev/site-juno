11f6adfb7c3f4398da27e475fc07259ac6faa57f
Tony Ko <tony@tko.dev>
2021-12-31 06:22:38 +0000

    Add next boilerplate

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // defaults
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'], // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  testEnvironment: 'jest-environment-jsdom',
+
+  // options
+  clearMocks: true,
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
