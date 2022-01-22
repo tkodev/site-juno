846f63d427142e12572af4c0b2305fc9109c13b4
Tony Ko <tony@tko.dev>
2022-01-22 06:35:17 +0000

    Add logo

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import { Box, Container, AppBar, Toolbar, Typography, Button } from '@mui/material'
 import { makeStyles } from '@mui/styles';
+import Image from 'next/image'
+import { Theme } from '@/configs/theme'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     gridRow: 1,
+  },
+  toolbar: {
+    padding: 0,
+  },
+  logo: {
+    height: 72,
+    marginLeft: theme.spacing(-1),
+    marginRight: theme.spacing(2),
   }
 }))
 
@@ -14,12 +24,22 @@ const Header: React.FC = (props) => {
   return (
     <header className={styles.root}>
       <AppBar position="relative" color="transparent">
-        <Toolbar>
+        <Container maxWidth='lg'>
+        <Toolbar className={styles.toolbar}>
+          <Box className={styles.logo}>
+            <Image
+              src="/logo-clear.png"
+              alt="Logo"
+              width="72"
+              height="72"
+            />
+          </Box>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Juno Site
           </Typography>
           <Button color="inherit" href="https://auth.tko.dev/">Login</Button>
         </Toolbar>
+        </Container>
       </AppBar>
     </header>
   )
